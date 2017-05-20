484fda4313db8aeaa4e208ec24585b9d32600227
Sundar Nagarajan <sun.nagarajan@gmail.com>
2017-05-19 17:08:54 -0700

    Reenable 01_install_firmware.sh
    Update scripts from bootutils repo

--- remaster/chroot/scripts/pkgs_missing_from.sh
+++ remaster/chroot/scripts/pkgs_missing_from.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 REQD_PKGS="$*"
 
-MISSING_PKGS=$(dpkg -l $REQD_PKGS | sed -e '1,4d'| grep -v '^ii' | awk '{printf("%s ", $2)}')
+MISSING_PKGS=$(dpkg -l $REQD_PKGS 2>/dev/null | sed -e '1,4d'| grep -v '^ii' | awk '{printf("%s ", $2)}')
+MISSING_PKGS="$MISSING_PKGS $(dpkg -l $REQD_PKGS 2>&1 1>/dev/null | sed -e 's/^dpkg-query: no packages found matching //')"
 if [ -n "${MISSING_PKGS%% }" ]; then
 	INSTALL_CMD="sudo apt-get install $MISSING_PKGS"
 else
