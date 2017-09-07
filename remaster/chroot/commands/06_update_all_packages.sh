4d7c5ebc028e9b20f7777f9a2eedb6d650a87c38
Sundar Nagarajan <sun.nagarajan@gmail.com>
2017-09-06 21:52:45 -0700

    Suppress output from 08_apt_cleanup.sh and 06_update_all_packages.sh

--- remaster/chroot/commands/06_update_all_packages.sh
+++ remaster/chroot/commands/06_update_all_packages.sh
@@ -3,4 +3,4 @@ PROG_PATH=${PROG_PATH:-$(readlink -e $0)}
 PROG_DIR=${PROG_DIR:-$(dirname ${PROG_PATH})}
 PROG_NAME=${PROG_NAME:-$(basename ${PROG_PATH})}
 
-(apt-get update && apt-get -y upgrade && apt-get dist-upgrade -y) 1>/dev/null
+(apt-get update && apt-get -y upgrade && apt-get dist-upgrade -y) 1>/dev/null 2>&1
--- remaster/chroot/commands/08_apt_cleanup.sh
+++ remaster/chroot/commands/08_apt_cleanup.sh
@@ -3,6 +3,6 @@ PROG_PATH=${PROG_PATH:-$(readlink -e $0)}
 PROG_DIR=${PROG_DIR:-$(dirname ${PROG_PATH})}
 PROG_NAME=${PROG_NAME:-$(basename ${PROG_PATH})}
 
-apt-get clean
-apt-get autoclean
-apt-get -y autoremove --purge
+apt-get clean 1>/dev/null 2>&1
+apt-get autoclean 1>/dev/null 2>&1
+apt-get -y autoremove --purge 1>/dev/null 2>&1
