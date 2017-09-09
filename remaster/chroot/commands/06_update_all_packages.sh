d24c860e282d55db25712875cdb31c08022aab05
Sundar Nagarajan <sun.nagarajan@gmail.com>
2017-09-08 20:44:36 -0700

    Add informative message to 06_update_all_packages.sh

--- remaster/chroot/commands/06_update_all_packages.sh
+++ remaster/chroot/commands/06_update_all_packages.sh
@@ -3,4 +3,6 @@ PROG_PATH=${PROG_PATH:-$(readlink -e $0)}
 PROG_DIR=${PROG_DIR:-$(dirname ${PROG_PATH})}
 PROG_NAME=${PROG_NAME:-$(basename ${PROG_PATH})}
 
+echo "Updating all packages - this may take quite a while"
+echo "- depending on your network speed and machine configuration"
 (apt-get update && apt-get -y upgrade && apt-get dist-upgrade -y) 1>/dev/null 2>&1
