2e7f1b94120d85ac7fa9865122cc999c53a16911
Sundar Nagarajan <sun.nagarajan@gmail.com>
2017-09-11 11:03:23 -0700

    Add all UCM files for Intel platforms

--- remaster/chroot/sound/hardware/sound/install.sh
+++ remaster/chroot/sound/hardware/sound/install.sh
@@ -3,35 +3,16 @@ PROG_DIR=$(printf %q "$(readlink -e $(dirname $0))")
 SCRIPTS_DIR=$(printf %q "${PROG_DIR}/scripts")
 
 # Check that required scripts are present
-if [ ! -d ${PROG_DIR}/bytcht-es8316 ]; then
-    echo "Missing dir: ${PROG_DIR}/bytcht-es8316"
+if [ ! -d ${PROG_DIR}/UCM ]; then
+    echo "Missing dir: ${PROG_DIR}/UCM"
     exit 1
 fi
-for f in bytcht-es8316.conf HiFi
-do
-    if [ ! -f ${PROG_DIR}/bytcht-es8316/$f ]; then
-        echo "Missing file: ${PROG_DIR}/bytcht-es8316/$f"
-        exit 1
-    fi
-done
-if [ ! -d ${PROG_DIR}/bytcr-rt5651 ]; then
-    echo "Missing dir: ${PROG_DIR}/bytcr-rt5651"
-    exit 1
-fi
-for f in asound.state bytcr-rt5651.conf HiFi
-do
-    if [ ! -f ${PROG_DIR}/bytcr-rt5651/$f ]; then
-        echo "Missing file: ${PROG_DIR}/bytcr-rt5651/$f"
-        exit 1
-    fi
-done
 if [ ! -f ${PROG_DIR}/rdp-sound-modules.conf ]; then
     echo "Missing file: ${PROG_DIR}/rdp-sound-modules.conf"
     exit 1
 fi
 
-\cp -frv ${PROG_DIR}/bytcht-es8316 /usr/share/alsa/ucm/
-\cp -frv ${PROG_DIR}/bytcr-rt5651 /usr/share/alsa/ucm/
+\cp -frv ${PROG_DIR}/UCM/* /usr/share/alsa/ucm/
 \cp -frv ${PROG_DIR}/rdp-sound-modules.conf /etc/modprobe.d/
 
 # On RDP Thinbook, set the default output (sink)
