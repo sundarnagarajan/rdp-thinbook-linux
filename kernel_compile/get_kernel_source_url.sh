8c882a5b889b69438e4c1c2b36b2ffa93b2efa95
Sundar Nagarajan <sun.nagarajan@gmail.com>
2017-10-18 11:00:02 -0700

    Move to kernel 4.13.8

--- kernel_compile/get_kernel_source_url.sh
+++ kernel_compile/get_kernel_source_url.sh
@@ -16,5 +16,5 @@
 # Note that for stable kernels, the url on kernel.org, that goes to
 # cdn.kernel.org may be faster TO DOWNLOAD (because of CDN)
 
-# 4.13.1 stable kernel from cdn.kernel.org
-echo "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.13.1.tar.xz"
+# 4.13.8 stable kernel from cdn.kernel.org
+echo "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.13.8.tar.xz"
