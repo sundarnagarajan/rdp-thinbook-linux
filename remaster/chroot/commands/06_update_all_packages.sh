5c21a6083bd8db2d1115c494c7cc63f05dd9bd02
Sundar Nagarajan <sun.nagarajan@gmail.com>
2017-10-26 17:30:56 -0700

    modified:   remaster/chroot/commands/06_update_all_packages.sh modified:   remaster/chroot/commands/07_install_grub_packages.sh
    On Ubuntu 17.10 systemd provides the system-wide DNS resolver
    On such distributions, /etc/resolv.conf inside the ISO points
    at ../run/systemd/resolve/stub-resolv.conf and the target will not
    exist IFF you are remastering on an older distribution
    
    We detect that there is no nameserver line in /etc/resolv.conf
    and if so, we move the existing /etc/resolv.conf aside and
    replace it with a file pointing at Google Public DNS
    At the end of the script we restore the original /etc/resolv.conf

--- remaster/chroot/commands/06_update_all_packages.sh
+++ remaster/chroot/commands/06_update_all_packages.sh
@@ -3,6 +3,33 @@ PROG_PATH=${PROG_PATH:-$(readlink -e $0)}
 PROG_DIR=${PROG_DIR:-$(dirname ${PROG_PATH})}
 PROG_NAME=${PROG_NAME:-$(basename ${PROG_PATH})}
 
+
+# On Ubuntu 17.10 systemd provides the system-wide DNS resolver
+# On such distributions, /etc/resolv.conf inside the ISO points
+# at ../run/systemd/resolve/stub-resolv.conf and the target will not
+# exist IFF you are remastering on an older distribution
+
+# We detect that there is no nameserver line in /etc/resolv.conf
+# and if so, we move the existing /etc/resolv.conf aside and 
+# replace it with a file pointing at Google Public DNS
+# At the end of the script we restore the original /etc/resolv.conf
+
+
+ORIG_RESOLV_CONF=/etc/resolv.conf.remaster_orig
+cat /etc/resolv.conf 2>/dev/null | grep -q '^nameserver'
+if [ $? -ne 0 ]; then
+    echo "Replacing /etc/resolv.conf"
+    mv /etc/resolv.conf $ORIG_RESOLV_CONF
+    echo "nameserver   8.8.8.8\nnameserver  8.8.4.4" > /etc/resolv.conf
+fi
+
 echo "Updating all packages - this may take quite a while"
 echo "- depending on your network speed and machine configuration"
 (apt-get update && apt-get -y upgrade && apt-get dist-upgrade -y) 1>/dev/null 2>&1
+
+# Restore original /etc/resolv.conf if we had moved it
+if [ -f  $ORIG_RESOLV_CONF ]; then
+    echo "Restoring original /etc/resolv.conf"
+    \rm -f /etc/resolv.conf
+    mv  $ORIG_RESOLV_CONF /etc/resolv.conf
+fi
