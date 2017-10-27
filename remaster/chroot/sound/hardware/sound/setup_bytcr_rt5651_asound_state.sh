51bef441cbead620144eca8cbaf49e297d16949b
Sundar Nagarajan <sun.nagarajan@gmail.com>
2017-10-26 19:41:06 -0700

    make setup_bytcr_rt5651_asound_state.sh smarter

--- remaster/chroot/sound/hardware/sound/setup_bytcr_rt5651_asound_state.sh
+++ remaster/chroot/sound/hardware/sound/setup_bytcr_rt5651_asound_state.sh
@@ -9,15 +9,28 @@ ASOUND_STATE_DIR=/usr/share/alsa/ucm/bytcr-rt5651
 ASOUND_SOURCE=${ASOUND_STATE_DIR}/asound.state
 ASOUND_TARGET=/var/lib/alsa/asound.state
 
-
-# Sleep a few secs - let rest of the system finish booting
-sleep 10
-
 # If there is no source file existing, cannot do anything anyway!
 if [ ! -f ${ASOUND_SOURCE} ]; then
     $LOGCMD "Source asound.state not found: ${ASOUND_SOURCE}"
     exit 1
 fi
+
+SLEEP_TIME=1
+SOUNDCARD_PREFIX="bytcr-rt5651"
+while true
+do
+    ALSA_SOUNDCARD=$(cat /proc/asound/cards | awk -F: '{print $2}' | awk -F' - ' '{if ($1 != "") {print $2} }' | egrep "^(${SOUNDCARD_PREFIX})")
+    if [ -n "$ALSA_SOUNDCARD" ]; then
+        if [ -n "$(ls -A /dev/snd/control* 2>/dev/null)" ]; then
+            echo "Found sound card: $SOUNDCARD_PREFIX"
+            break
+        fi
+    fi
+    sleep $SLEEP_TIME
+done
+
+# Sleep a few secs - let rest of the system finish booting
+sleep 2
 
 # Check that bytcr_rt5651 sound card is present and activated in kernel
 dmesg | fgrep -q "$DMESG_INDICATOR"
@@ -35,6 +48,7 @@ if [ -f ${ASOUND_TARGET} ]; then
 fi
 
 # If we got here ASOUND_TARGET is not present OR we got the -f option
+mkdir -p $(dirname ${ASOUND_TARGET})
 \cp -f ${ASOUND_SOURCE} ${ASOUND_TARGET}
 /usr/sbin/alsactl restore 2>&1 | $LOGCMD
 exit $?
