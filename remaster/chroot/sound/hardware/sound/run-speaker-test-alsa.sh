80f1c6d6ebca616ab6173509e14d7c88a9637555
Sundar Nagarajan <sun.nagarajan@gmail.com>
2017-10-26 17:42:32 -0700

    match bytcht-es8316 and bytcr-rt5651

--- remaster/chroot/sound/hardware/sound/run-speaker-test-alsa.sh
+++ remaster/chroot/sound/hardware/sound/run-speaker-test-alsa.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-SOUNDCARD_PREFIX=bytcht
-ALSA_SOUNDCARD=$(cat /proc/asound/cards | awk -F: '{print $2}' | awk -F' - ' '{if ($1 != "") {print $2} }' | grep "^${SOUNDCARD_PREFIX}")
+SOUNDCARD_PREFIX="bytcht-es8316|bytcr-rt5651"
+ALSA_SOUNDCARD=$(cat /proc/asound/cards | awk -F: '{print $2}' | awk -F' - ' '{if ($1 != "") {print $2} }' | egrep "^(${SOUNDCARD_PREFIX})")
 if [ -z "$ALSA_SOUNDCARD" ]; then
-    echo "No sound card matching $SOUNDCARD_PREFIX found"
+    echo "No sound card found matching $SOUNDCARD_PREFIX"
     exit 1
 fi
 echo "Using sound card $ALSA_SOUNDCARD"
