54961a4c055db3619ffc98bb2f359ea9ef99e555
Sundar Nagarajan <sun.nagarajan@gmail.com>
2017-11-17 04:54:04 -0800

    cleanup of make_rdp_iso.sh into separate functions

--- make_rdp_iso.sh
+++ make_rdp_iso.sh
@@ -31,41 +31,57 @@ function check_avail_disk_space {
     echo ""
 }
 
+function update_from_git {
+    cd $TOP_DIR
+    rm -rf bootutils rdp-thinbook-linux
+    echo "Cloning bootutils..."
+    git clone --depth 1 https://github.com/sundarnagarajan/bootutils.git 2>/dev/null
+    echo "Cloning rdp-thinbook-linux..."
+    git clone --depth 1 https://github.com/sundarnagarajan/rdp-thinbook-linux.git 2>/dev/null
+}
+
+function compile_kernel {
+    cd $TOP_DIR
+    cp -r rdp-thinbook-linux/kernel_compile $TOP_DIR/
+    cd $TOP_DIR/kernel_compile
+    ./patch_and_build_kernel.sh
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+    mv debs/*.deb $TOP_DIR/rdp-thinbook-linux/remaster/chroot/kernel-debs/
+
+    cd $TOP_DIR
+    rm -rf kernel_compile
+}
+
+function remaster_iso {
+    if [ $(id -u) -ne 0 ]; then
+        echo "Must be run as root"
+        exit 1
+    fi
+    if [ ! -f "$INPUT_ISO" ]; then
+        echo "INPUT_ISO not found: $INPUT_ISO"
+        exit 1
+    fi
+    
+    if [ -n "${OUTPUT_ISO}" -a -f "${OUTPUT_ISO}" ]; then
+        sudo rm -f ${OUTPUT_ISO}
+    fi
+    sudo REMASTER_CMDS_DIR=${R_DIR} ${TOP_DIR}/bootutils/scripts/ubuntu_remaster_iso.sh ${INPUT_ISO} ${EXTRACT_DIR} ${OUTPUT_ISO}
+}
+
 
 START_TIME=$(date)
-TOP_DIR=$(pwd)
-R_DIR=${TOP_DIR}/rdp-thinbook-linux/remaster
-INPUT_ISO=${TOP_DIR}/ISO/in/source.iso
-EXTRACT_DIR=${TOP_DIR}/ISO/extract
-OUTPUT_ISO=${TOP_DIR}/ISO/out/modified.iso
+export TOP_DIR=$(readlink -e $(dirname $0))
+export R_DIR=${TOP_DIR}/rdp-thinbook-linux/remaster
+export INPUT_ISO=${TOP_DIR}/ISO/in/source.iso
+export EXTRACT_DIR=${TOP_DIR}/ISO/extract
+export OUTPUT_ISO=${TOP_DIR}/ISO/out/modified.iso
 
 check_required_pkgs
 check_avail_disk_space
-if [ $(id -u) -ne 0 ]; then
-    echo "Must be run as root"
-    exit 1
-fi
-if [ ! -f "$INPUT_ISO" ]; then
-    echo "INPUT_ISO not found: $INPUT_ISO"
-    exit 1
-fi
 
-rm -rf bootutils rdp-thinbook-linux "$OUTPUT_ISO"
-echo "Cloning bootutils..."
-git clone --depth 1 https://github.com/sundarnagarajan/bootutils.git 2>/dev/null
-echo "Cloning rdp-thinbook-linux..."
-git clone --depth 1 https://github.com/sundarnagarajan/rdp-thinbook-linux.git 2>/dev/null
-
-cp -r rdp-thinbook-linux/kernel_compile $TOP_DIR/
-cd $TOP_DIR/kernel_compile
-./patch_and_build_kernel.sh
-if [ $? -ne 0 ]; then
-    exit 1
-fi
-mv debs/*.deb $TOP_DIR/rdp-thinbook-linux/remaster/chroot/kernel-debs/
-
-cd $TOP_DIR
-rm -rf kernel_compile
-
-sudo REMASTER_CMDS_DIR=${R_DIR} ${TOP_DIR}/bootutils/scripts/ubuntu_remaster_iso.sh ${INPUT_ISO} ${EXTRACT_DIR} ${OUTPUT_ISO}
+update_from_git
+compile_kernel
+remaster_iso
 echo "Start: $START_TIME" ; echo "Ended: $(date)"
