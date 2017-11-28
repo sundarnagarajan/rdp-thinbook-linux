86e49b2344647b1c30dee5ce4e345860306d340c
Sundar Nagarajan <sun.nagarajan@gmail.com>
2017-11-27 17:49:09 -0800

    Add curl to required packages

--- required_pkgs.sh
+++ required_pkgs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-REQD_PKGS="grub-efi-ia32-bin grub-efi-amd64-bin grub-pc-bin grub2-common grub-common util-linux parted gdisk mount xorriso genisoimage squashfs-tools rsync git build-essential kernel-package fakeroot libncurses5-dev libssl-dev ccache libfile-fcntllock-perl"
+REQD_PKGS="grub-efi-ia32-bin grub-efi-amd64-bin grub-pc-bin grub2-common grub-common util-linux parted gdisk mount xorriso genisoimage squashfs-tools rsync git build-essential kernel-package fakeroot libncurses5-dev libssl-dev ccache libfile-fcntllock-perl curl"
 
 $(dirname $0)/pkgs_missing_from.sh $REQD_PKGS
