f331dbebfa2ce88dfcde30de3eeb20946f12d5ca
Sundar Nagarajan <sun.nagarajan@gmail.com>
2018-07-04 01:44:21 -0700

    Fix resolv.conf

--- remaster/chroot/commands/03_install_firmware.sh
+++ remaster/chroot/commands/03_install_firmware.sh
@@ -6,19 +6,44 @@ PROG_PATH=${PROG_PATH:-$(readlink -e $0)}
 PROG_DIR=${PROG_DIR:-$(dirname ${PROG_PATH})}
 PROG_NAME=${PROG_NAME:-$(basename ${PROG_PATH})}
 
-# Fetch the latest firmware from linux firmware git instead
-apt-get update && apt-get install git -y
+LINUX_FIRMWARE_GIT='git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git'
+# On Ubuntu 17.10 systemd provides the system-wide DNS resolver
+# On such distributions, /etc/resolv.conf inside the ISO points
+# at ../run/systemd/resolve/stub-resolv.conf and the target will not
+# exist IFF you are remastering on an older distribution
+
+# We detect that there is no nameserver line in /etc/resolv.conf
+# and if so, we move the existing /etc/resolv.conf aside and 
+# replace it with a file pointing at Google Public DNS
+# At the end of the script we restore the original /etc/resolv.conf
+
+
+ORIG_RESOLV_CONF=/etc/resolv.conf.remaster_orig
+cat /etc/resolv.conf 2>/dev/null | grep -q '^nameserver'
 if [ $? -ne 0 ]; then
-    echo "Could not install git"
-    exit 0
+    echo "Replacing /etc/resolv.conf"
+    mv /etc/resolv.conf $ORIG_RESOLV_CONF
+    echo -e "nameserver   8.8.8.8\nnameserver  8.8.4.4" > /etc/resolv.conf
 fi
 
-LINUX_FIRMWARE_GIT='git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git'
+apt-get update 1>/dev/null
+# Fetch the latest firmware from linux firmware git instead
+apt-get -y install git
+if [ $? -ne 0 ]; then
+	apt-get -f install
+fi
+
 cd /lib
 rm -rf firmware
 git clone --depth 1 $LINUX_FIRMWARE_GIT firmware
 rm -rf firmware/.git
-apt-get autoremove --purge git
+apt-get autoremove -y --purge git
+# Restore original /etc/resolv.conf if we had moved it
+if [ -f  $ORIG_RESOLV_CONF -o -L $ORIG_RESOLV_CONF ]; then
+    echo "Restoring original /etc/resolv.conf"
+    \rm -f /etc/resolv.conf
+    mv  $ORIG_RESOLV_CONF /etc/resolv.conf
+fi
 
 exit 0
 
