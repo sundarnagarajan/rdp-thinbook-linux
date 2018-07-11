7eebe1d159950672de4d64ce704c53642b10b737
Sundar Nagarajan <sun.nagarajan@gmail.com>
2018-07-11 15:58:27 -0700

    Smarter way to update ISo kernel

--- remaster/iso_post/commands/01_update_iso_kernel.sh
+++ remaster/iso_post/commands/01_update_iso_kernel.sh
@@ -22,14 +22,56 @@ if [ ! -d "$REMASTER_ISO_CHROOT_DIR" ]; then
     exit 0
 fi
 
-if [ ! -f ${REMASTER_ISO_CHROOT_DIR}/${REMASTER_DIR}/${KP_LIST} ]; then
-    echo "Kernel not updated"
+cd ${REMASTER_ISO_CHROOT_DIR}/boot/
+# Get highest version vmlinuz and initrd
+SRC_VMLINUZ=$(ls vmlinuz-*| tail -1)
+if [ -z "$SRC_VMLINUZ" ]; then
+    echo "No vmlinuz found under $(pwd)"
     exit 0
 fi
+SRC_VER=$(echo $SRC_VMLINUZ | cut -d- -f2-)
+if [ -z "$SRC_VER" ]; then
+    echo "vmlinuz did not contain version: $(pwd)/$SRC_VMLINUZ"
+    exit 0
+fi
+SRC_INITRD=initrd.img-${SRC_VER}
+if [ ! -f "$SRC_INITRD" ]; then
+    echo "initrd not found: $(pwd)/$SRC_INITRD"
+    exit 0
+fi
+echo "Using vmlinuz: $SRC_VMLINUZ"
+echo "Using initrd: $SRC_INITRD"
+
+SRC_VMLINUZ="$(pwd)/$SRC_VMLINUZ"
+SRC_INITRD="$(pwd)/$SRC_INITRD"
+for f in $SRC_VMLINUZ $SRC_INITRD
+do
+    if [ ! -f "$f" ]; then
+        echo "file not found: $f"
+        exit 0
+    fi
+done
 
 # On 18.04 grub.cfg references vmlinuz and not vmlinuz.efi
-if [ -f ${ISO_EXTRACT_DIR}/casper/vmlinuz ]; then
-    \cp ${REMASTER_ISO_CHROOT_DIR}/boot/vmlinuz-* ${ISO_EXTRACT_DIR}/casper/vmlinuz
+if [ -f ${ISO_EXTRACT_DIR}/casper/vmlinuz.efi ]; then
+    TARGET_VMLINUZ=${ISO_EXTRACT_DIR}/casper/vmlinuz.efi
+elif [ -f ${ISO_EXTRACT_DIR}/casper/vmlinuz ]; then
+    TARGET_VMLINUZ=${ISO_EXTRACT_DIR}/casper/vmlinuz
+else
+    echo "Could not find target vmlinuz"
+    exit 0
 fi
-\cp ${REMASTER_ISO_CHROOT_DIR}/boot/vmlinuz-* ${ISO_EXTRACT_DIR}/casper/vmlinuz.efi
-\cp ${REMASTER_ISO_CHROOT_DIR}/boot/initrd.img-* ${ISO_EXTRACT_DIR}/casper/initrd.lz
+TARGET_INITRD=${ISO_EXTRACT_DIR}/casper/initrd.lz
+
+diff --brief $SRC_VMLINUZ $TARGET_VMLINUZ 1>/dev/null
+if [ $? -eq 0 ]; then
+    echo "vmlinuz unchanged - not overwriting"
+else
+    \cp -f $SRC_VMLINUZ $TARGET_VMLINUZ
+fi
+diff --brief $SRC_INITRD $TARGET_INITRD 1>/dev/null
+if [ $? -eq 0 ]; then
+    echo "initrd unchanged - not overwriting"
+else
+    \cp -f $SRC_INITRD $TARGET_INITRD
+fi
