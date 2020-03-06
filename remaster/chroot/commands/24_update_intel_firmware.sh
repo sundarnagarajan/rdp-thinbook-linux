3d022e109efa4d4c957d6c51f4be8b0c53c2648b
Sundar Nagarajan <sun.nagarajan@gmail.com>
2020-03-05 17:39:44 -0800

    Separate update of linux and intel firmware into separate scripts

--- remaster/chroot/commands/24_update_intel_firmware.sh
+++ remaster/chroot/commands/24_update_intel_firmware.sh
@@ -6,7 +6,6 @@ PROG_PATH=${PROG_PATH:-$(readlink -e $0)}
 PROG_DIR=${PROG_DIR:-$(dirname ${PROG_PATH})}
 PROG_NAME=${PROG_NAME:-$(basename ${PROG_PATH})}
 
-LINUX_FIRMWARE_GIT='git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git'
 INTEL_FIRMWARE_GIT='git://git.kernel.org/pub/scm/linux/kernel/git/iwlwifi/linux-firmware.git'
 
 # On Ubuntu 17.10 systemd provides the system-wide DNS resolver
@@ -29,19 +28,12 @@ if [ $? -ne 0 ]; then
 fi
 
 apt-get update 1>/dev/null
-# Fetch the latest firmware from linux firmware git instead
 apt-get -y install git 1>/dev/null
 if [ $? -ne 0 ]; then
     echo "Install failed: git"
     exit 1
 fi
 
-cd /lib
-rm -rf firmware
-
-# Linux firmware git
-git clone --quiet --depth 1 $LINUX_FIRMWARE_GIT firmware 2>&1
-rm -rf firmware/.git
 # Intel firmware git
 git clone --quiet --depth 1 $INTEL_FIRMWARE_GIT intel-firmware 2>&1
 rm -rf intel-firmware/.git
@@ -60,23 +52,3 @@ if [ -f  $ORIG_RESOLV_CONF -o -L $ORIG_RESOLV_CONF ]; then
     \rm -f /etc/resolv.conf
     mv  $ORIG_RESOLV_CONF /etc/resolv.conf
 fi
-
-# Rest was used earlier to copy RTL8723bs firmware - not needed any more
-# if we get latest linux firmware
-exit 0
-
-FIRMWARE_SRC_DIR=${PROG_DIR}/../firmware
-FIRMWARE_DEST_DIR=/lib/firmware
-
-if [ ! -d ${FIRMWARE_SRC_DIR} ]; then
-    echo "FIRMWARE_SRC_DIR not a directory: $FIRMWARE_SRC_DIR"
-    exit 0
-fi
-FIRMWARE_SRC_DIR=$(readlink -e $FIRMWARE_SRC_DIR)
-test "$(ls -A $FIRMWARE_SRC_DIR)"
-if [ $? -ne 0 ]; then
-    echo "No firmware files to copy: $FIRMWARE_SRC_DIR"
-    exit 0
-fi
-
-cp -r ${FIRMWARE_SRC_DIR}/. $FIRMWARE_DEST_DIR/
