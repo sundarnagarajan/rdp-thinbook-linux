c4d3398314490c13c4a23712a48e684e30500be3
Sundar Nagarajan <sun.nagarajan@gmail.com>
2020-04-01 18:08:26 -0700

    virt_what_check_cmd.sh: check lscpu/hypervisor ALSO

--- remaster/chroot/virtualbox/virtualbox/scripts/virt_what_check_cmd.sh
+++ remaster/chroot/virtualbox/virtualbox/scripts/virt_what_check_cmd.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 VIRT_WHAT=$(/usr/bin/which virt-what 2>/dev/null)
 if [ -z "$VIRT_WHAT" ]; then
-    >&2 echo "virt-what not found"
-    exit 0
+    # Try checking output of lscpu instead
+    if [ -x /usr/bin/lscpu ]; then
+        /usr/bin/lscpu | grep -q hypervisor
+        ret=$?
+        if [ $ret -eq 0 ]; then
+            >&2 echo "Running in hypervisor; starting $(basename $0)"
+        else
+            >&2 echo "Running in hypervisor; NOT starting $(basename $0)"
+        fi
+        return $ret
+    else
+        >&2 echo "Neither virt-what nor lscpu not found"
+        exit 0
+    fi
 fi
 $VIRT_WHAT | grep -q virtualbox
 ret=$?
 if [ $ret -eq 0 ]; then
-    >&2 echo "Running in virtualbox"
-    >&2 echo "$(basename $0) : OK"
+    >&2 echo "Running in virtualbox; starting $(basename $0)"
 else
-    >&2 echo "Not running in virtualbox"
-    >&2 echo "$(basename $0) : Not started"
+    >&2 echo "Not running in virtualbox; NOT starting $(basename $0)"
 fi
 exit $ret
