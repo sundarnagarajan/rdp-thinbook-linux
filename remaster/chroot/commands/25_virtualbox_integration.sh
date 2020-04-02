a76f30dcfd9b447cf7f640391d509d151add1141
Sundar Nagarajan <sun.nagarajan@gmail.com>
2020-04-01 22:18:34 -0700

    Download virtualbox-guest-dkms DEB - fixes

--- remaster/chroot/commands/25_virtualbox_integration.sh
+++ remaster/chroot/commands/25_virtualbox_integration.sh
@@ -43,10 +43,10 @@ function install_virtualbox_guest_dkms() {
     local EXISTING_DEB="${VBOX_DIR}/virtualbox-guest-dkms.deb"
     if [ -f "$EXISTING_DEB" ]; then
         echo "Installing downloaded DEB: $EXISTING_DEB"
-        dpkg -i "$EXISTING_DEB" 1>/dev/null 2>&1
+        dpkg -i "$EXISTING_DEB"
         if [ $? -ne 0 ]; then
-            apt-get -f install --no-install-recommends --no-install-suggests 1>/dev/null 2>&1
-            return 1
+            apt-get -f install --no-install-recommends --no-install-suggests
+            return $?
         fi
     else
         echo "DEB not found: $EXISTING_DEB"
@@ -57,7 +57,6 @@ function install_virtualbox_guest_dkms() {
         local ret=$?
         if [ $ret -ne 0 ]; then
             echo "Install failed: $REQUIRED_PKGS"
-            return 1
             apt-get -f install 2>/dev/null
             return 1
         fi
