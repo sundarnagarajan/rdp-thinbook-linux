9bcb12ba45b78bf5338332308ff363af51f50b36
Sundar Nagarajan <sun.nagarajan@gmail.com>
2021-04-16 14:08:27 -0700

    Eradicate DEBUG_REDIRECT

--- remaster/chroot/commands/310_linux_firmware.sh
+++ remaster/chroot/commands/310_linux_firmware.sh
@@ -23,8 +23,6 @@ PROG_PATH=${PROG_PATH:-$(readlink -e $0)}
 PROG_DIR=${PROG_DIR:-$(dirname ${PROG_PATH})}
 PROG_NAME=${PROG_NAME:-$(basename ${PROG_PATH})}
 FAILED_EXIT_CODE=1
-DEBUG_REDIRECT=''
-DEBUG_REDIRECT='1>/dev/null 2>&1'
 
 
 set -eu -o pipefail
@@ -79,13 +77,12 @@ function cleanup_firmware_dirs(){
     rm -rf $FIRMWARE_DIR_LINUX_NEW $FIRMWARE_DIR_INTEL_NEW    
     [[ "$GIT_REMOVE_REQUIRED" = "yes" ]] && {
         echo "Uninstalling git"
-        apt-get autoremove -y --purge git $DEBUG_REDIRECT
     }
 }
 
 function update_firmware_package(){
     echo "Updating linux-firmware package"
-    apt upgrade -y linux-firmware $DEBUG_REDIRECT || {
+    apt upgrade -y linux-firmware 1>/dev/null 2>&1 || {
         echo "linux-firmware upgrade failed"
         return 1
     }
@@ -100,7 +97,7 @@ function install_git_if_required(){
     local GIT_ALREADY_INSTALLED=$(dpkg-query -W --showformat='${Package}\n' | fgrep -x git)
     if [ -z "$GIT_ALREADY_INSTALLED" ]; then
         echo "Installing git"
-        apt-get -y install --no-install-recommends --no-install-suggests git $DEBUG_REDIRECT || {
+        apt-get -y install --no-install-recommends --no-install-suggests git $1>/dev/null 2>&1 || {
             echo "Install failed: git"
             return 1
         }
@@ -111,7 +108,7 @@ function install_git_if_required(){
 function update_firmware_linux_firmware_git(){
     local LINUX_FIRMWARE_GIT='https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git'
     rm -rf $FIRMWARE_DIR_LINUX_NEW
-    git clone --depth 1 "$LINUX_FIRMWARE_GIT" $FIRMWARE_DIR_LINUX_NEW $DEBUG_REDIRECT || {
+    git clone --depth 1 "$LINUX_FIRMWARE_GIT" $FIRMWARE_DIR_LINUX_NEW 1>/dev/null 2>&1 || {
         echo "git clone of linux-firmware git failed"
         return 1
     }
@@ -123,7 +120,7 @@ function update_firmware_intel_firmware_git(){
     # dir FIRMWARE_DIR_LINUX_NEW was already created
     local INTEL_FIRMWARE_GIT='https://git.kernel.org/pub/scm/linux/kernel/git/iwlwifi/linux-firmware.git'
     rm -rf $FIRMWARE_DIR_INTEL_NEW
-    git clone --depth 1 "$INTEL_FIRMWARE_GIT" $FIRMWARE_DIR_INTEL_NEW $DEBUG_REDIRECT || {
+    git clone --depth 1 "$INTEL_FIRMWARE_GIT" $FIRMWARE_DIR_INTEL_NEW 1>/dev/null 2>&1 || {
         echo "git clone of intel-firmware git failed"
         return 1
     }
