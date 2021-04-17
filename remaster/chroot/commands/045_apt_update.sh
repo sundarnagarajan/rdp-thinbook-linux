e50b2cbd22c4c7601552430e55da82012381f2b6
Sundar Nagarajan <sun.nagarajan@gmail.com>
2021-04-16 18:01:29 -0700

    Add universe repository

--- remaster/chroot/commands/045_apt_update.sh
+++ remaster/chroot/commands/045_apt_update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Run 'apt update' to get current package lists
 # Requires Internet, updated /etc/resolv.conf (020_set_dns.sh)
+add-apt-repository -n -y universe 1>/dev/null 2>&1 && {
+    echo "Added universe repository"
+}
 apt update 1>/dev/null 2>&1
