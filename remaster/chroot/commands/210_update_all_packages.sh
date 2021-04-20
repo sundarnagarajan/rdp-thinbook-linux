1defbf2481cd68b95404ec6df259155b98a86455
Sundar Nagarajan <sun.nagarajan@gmail.com>
2021-04-20 14:43:24 -0700

    Hide apt messages

--- remaster/chroot/commands/210_update_all_packages.sh
+++ remaster/chroot/commands/210_update_all_packages.sh
@@ -6,6 +6,6 @@ PROG_DIR=${PROG_DIR:-$(dirname ${PROG_PATH})}
 PROG_NAME=${PROG_NAME:-$(basename ${PROG_PATH})}
 
 
-echo "Updating all packages - this may take quite a while"
-echo "- depending on your network speed and machine configuration"
-(apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y)
+echo "Updating all packages - this may take quite a while depending on your"
+echo "network speed and machine configuration"
+(apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y) 1>/dev/null 2>&1
