e5fdd723ae6cf0ed014e5d49d5fe7134c40c5efb
Sundar Nagarajan <sun.nagarajan@gmail.com>
2021-04-22 12:59:46 -0700

    Use update_firmware.sh

--- remaster/chroot/commands/320_linux_firmware.sh
+++ remaster/chroot/commands/320_linux_firmware.sh
@@ -6,14 +6,16 @@
 # 3 guard variables - all can be 'no (default) or 'yes:
 # FIRMWARE_UPDATE_PACKAGE:
 #   - update linux-firmware PACKAGE from repository
-#   - Meaningless if FIRMWARE_UPDATE_FIRMWARE_GIT is set
-# FIRMWARE_UPDATE_FIRMWARE_GIT:
-#   - Pull firmware directly from linux-firmware git repo
-#   - If this is set, FIRMWARE_UPDATE_PACKAGE is IGNORED
+#   - If this is set, the following are IGNORED:
+#       FIRMWARE_UPDATE_FIRMWARE_GIT_UBUNTU
+#       FIRMWARE_UPDATE_FIRMWARE_GIT_LINUX
+#       FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL
+# FIRMWARE_UPDATE_FIRMWARE_GIT_UBUNTU:
+#   - Pull firmware from ubuntu-firmware git repo
+# FIRMWARE_UPDATE_FIRMWARE_GIT_LINUX:
+#   - Pull firmware from linux-firmware git repo
 # FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL:
-#   - Pull Intel firmware (ONLY) from iwlwifi firmware git
-#   - FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL implies FIRMWARE_UPDATE_FIRMWARE_GIT
-#       and automatically ignores FIRMWARE_UPDATE_PACKAGE
+#   - Pull Intel firmware from iwlwifi firmware git
 #
 # Requires 020_set_dns.sh if ANY of the guard variables are set to "yes"
 # Requires 045_apt_update.sh if FIRMWARE_UPDATE_PACKAGE = "yes"
@@ -25,7 +27,8 @@
 #
 # Restrict to setting:
 #   FIRMWARE_UPDATE_PACKAGE
-#   FIRMWARE_UPDATE_FIRMWARE_GIT
+#   FIRMWARE_UPDATE_FIRMWARE_GIT_UBUNTU
+#   FIRMWARE_UPDATE_FIRMWARE_GIT_LINUX
 #   FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL
 #
 # Luckily commands in this directory are executed within the chroot, so
@@ -41,18 +44,24 @@ APT_CMD=apt-get
 REMASTER_DIR=/root/remaster
 FAILED_EXIT_CODE=1
 
-
 set -eu -o pipefail
+
+UPDATE_FIRMWARE_DIR=${PROG_DIR}/../update_firmware
+UPDATE_FIRMWARE_SCRIPT=${UPDATE_FIRMWARE_DIR}/update_firmware.sh
+UPDATE_FIRMWARE_SCRIPT=$(readlink -m "$UPDATE_FIRMWARE_SCRIPT")
+[[ -f "$UPDATE_FIRMWARE_SCRIPT" ]] || {
+    echo "Firmware update script not found: $UPDATE_FIRMWARE_SCRIPT"
+    exit $FAILED_EXIT_CODE
+}
 
 
 FIRMWARE_UPDATE_PACKAGE=no
-FIRMWARE_UPDATE_FIRMWARE_GIT=no
-FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL=no
+FIRMWARE_UPDATE_FIRMWARE_GIT_UBUNTU=yes
+FIRMWARE_UPDATE_FIRMWARE_GIT_LINUX=yes
+FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL=yes
+# TODO: add code to source prefs file
 # Was git installed in this script
 GIT_REMOVE_REQUIRED=no
-# Temporary firmware dirs
-FIRMWARE_DIR_LINUX_NEW=/lib/firmware-new
-FIRMWARE_DIR_INTEL_NEW=/lib/firmware-intel
 
 
 # Utility function that compares 2 version values and returns 0 if $2 >= $1
@@ -65,33 +74,20 @@ function compare_versions() {
 
 # Function that sets the 3 guard variables
 function set_opts(){
-    # Following lines are where we set the guard variables
-    FIRMWARE_UPDATE_PACKAGE=yes
-    FIRMWARE_UPDATE_FIRMWARE_GIT=yes
-    FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL=yes
-
-    # Override (if desired) based on current Ubuntu release and / or current kernel version
-    local MIN_RELEASE=20.10
-    local MIN_KERNEL=5.8
-    local CUR_RELEASE=$(cat /etc/os-release | grep '^VERSION_ID' | cut -d= -f2 | sed -e 's/^"//' -e 's/"$//')
-    local MAX_KERNEL_VER_INSTALLED=$(dpkg -l 'linux-image*' | grep '^ii' | awk '{print $3}' |sort -Vr | head -1)
-
-    compare_versions $MIN_RELEASE $CUR_RELEASE && compare_versions $MIN_KERNEL $MAX_KERNEL_VER_INSTALLED && {
-        # echo "Current kernel (${MAX_KERNEL_VER_INSTALLED}) meets minimum requirements (${MIN_KERNEL})"
-        # echo "Current release (${CUR_RELEASE}) meets minimum release (${MIN_RELEASE})"
-        printf ""    # Null operation
-    }
 
     # Remaining should be sane dependencies
-    [[ "$FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL" = "yes" ]] && {
-        FIRMWARE_UPDATE_FIRMWARE_GIT=yes
-        FIRMWARE_UPDATE_PACKAGE=no
+    [[ "$FIRMWARE_UPDATE_PACKAGE" = "yes" ]] && {
+        FIRMWARE_UPDATE_FIRMWARE_GIT_UBUNTU="no"
+        FIRMWARE_UPDATE_FIRMWARE_GIT_LINUX="no"
+        FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL="no"
+        return
     }
-    [[ "$FIRMWARE_UPDATE_FIRMWARE_GIT" = "yes" ]] && FIRMWARE_UPDATE_PACKAGE=no
+    [[ "$FIRMWARE_UPDATE_FIRMWARE_GIT_UBUNTU" = "yes" ]] && FIRMWARE_UPDATE_PACKAGE=no
+    [[ "$FIRMWARE_UPDATE_FIRMWARE_GIT_LINUX" = "yes" ]] && FIRMWARE_UPDATE_PACKAGE=no
+    [[ "$FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL" = "yes" ]] && FIRMWARE_UPDATE_PACKAGE=no
 }
 
-function cleanup_firmware_dirs(){
-    rm -rf $FIRMWARE_DIR_LINUX_NEW $FIRMWARE_DIR_INTEL_NEW || true
+function cleanup_uninstall_git() {
     [[ "$GIT_REMOVE_REQUIRED" = "yes" ]] && {
         echo "Uninstalling git"
         DEBIAN_FRONTEND=noninteractive $APT_CMD autoremove --purge git 1>/dev/null 2>&1 || true
@@ -123,88 +119,6 @@ function install_git_if_required(){
     fi
 }
 
-function update_firmware_linux_firmware_git(){
-    local LINUX_FIRMWARE_GIT='https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git'
-    rm -rf $FIRMWARE_DIR_LINUX_NEW
-    git clone --depth 1 "$LINUX_FIRMWARE_GIT" $FIRMWARE_DIR_LINUX_NEW 1>/dev/null 2>&1 || {
-        echo "git clone of linux-firmware git failed"
-        return 1
-    }
-    echo "Updated firmware from linux firmware git"
-}
-
-function update_firmware_intel_firmware_git(){
-    # ASSUMES that update_firmware_linux_firmware_git would have been called and
-    # dir FIRMWARE_DIR_LINUX_NEW was already created
-    local INTEL_FIRMWARE_GIT='https://git.kernel.org/pub/scm/linux/kernel/git/iwlwifi/linux-firmware.git'
-    rm -rf $FIRMWARE_DIR_INTEL_NEW
-    git clone --depth 1 "$INTEL_FIRMWARE_GIT" $FIRMWARE_DIR_INTEL_NEW 1>/dev/null 2>&1 || {
-        echo "git clone of intel-firmware git failed"
-        return 1
-    }
-
-    
-    # iwlwifi-*.ucode
-    echo "Updating iwlwifi microcode"
-    for f in $FIRMWARE_DIR_INTEL_NEW/iwlwifi-*.ucode
-    do
-        f=$(echo "$f" | sed -e 's/^\.\///')
-        local bn=$(basename $f)
-        [[ -f $FIRMWARE_DIR_LINUX_NEW/$bn ]] && {
-            diff --brief $f $FIRMWARE_DIR_LINUX_NEW/$bn || {
-                # Users will not be interested in actual firmware files updated
-                # \cp -f $f $FIRMWARE_DIR_LINUX_NEW/$bn && echo "    $f" || {
-                \cp -f $f $FIRMWARE_DIR_LINUX_NEW/$bn || {
-                    echo "Copy failed: $f"
-                    cd "$oldpwd"
-                    return 1
-                }
-            }
-        } || {
-            # Users will not be interested in actual firmware files updated
-            # \cp -f $f $FIRMWARE_DIR_LINUX_NEW/$bn && echo "    $f" || {
-            \cp -f $f $FIRMWARE_DIR_LINUX_NEW/$bn || {
-                echo "Copy failed: $f"
-                cd "$oldpwd"
-                return 1
-            }
-        }
-    done
-    
-    # intel/*
-    echo 'Updating intel-firmware/intel/'
-    [[ -d $FIRMWARE_DIR_LINUX_NEW/intel ]] || {
-        mkdir -p $FIRMWARE_DIR_LINUX_NEW/intel
-    }
-    cd $FIRMWARE_DIR_INTEL_NEW/intel
-    for f in $(find -type f)
-    do
-        f=$(echo "$f" | sed -e 's/^\.\///')
-        [[ -f $FIRMWARE_DIR_LINUX_NEW/$f ]] && {
-            diff --brief $f $FIRMWARE_DIR_LINUX_NEW/$f || {
-                # Users will not be interested in actual firmware files updated
-                # \cp --parents -f $f $FIRMWARE_DIR_LINUX_NEW/ && echo "    $f" || {
-                \cp --parents -f $f $FIRMWARE_DIR_LINUX_NEW/ || {
-                    echo "Copy failed: $f"
-                    return 1
-                }
-            }
-        } || {
-            # Users will not be interested in actual firmware files updated
-            # \cp --parents -f $f $FIRMWARE_DIR_LINUX_NEW/ && echo "    $f" || {
-            \cp --parents -f $f $FIRMWARE_DIR_LINUX_NEW/ || {
-                echo "Copy failed: $f"
-                return 1
-            }
-        }
-    done
-
-    cd /lib
-    \rm -rf  $FIRMWARE_DIR_INTEL_NEW
-    echo "Updated firmware from iwlwifi firmware git"
-}
-
-
 # ------------------------------------------------------------------------
 # Actual script starts after this
 # ------------------------------------------------------------------------
@@ -218,42 +132,17 @@ set_opts
 }
 
 # Setup trap to cleanup
-trap cleanup_firmware_dirs 1 2 3 15
+trap cleanup_uninstall_git 1 2 3 15
 
 
 # Install git if required and not installed
-[[ "$FIRMWARE_UPDATE_FIRMWARE_GIT" = "yes" || "$FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL" = "yes" ]] && {
-    install_git_if_required || exit $FAILED_EXIT_CODE
-}
-
-[[ "$FIRMWARE_UPDATE_FIRMWARE_GIT" = "yes" ]] && {
-    update_firmware_linux_firmware_git || exit $FAILED_EXIT_CODE
-}
-
-[[ "$FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL" = "yes" ]] && {
-    update_firmware_intel_firmware_git || exit $FAILED_EXIT_CODE
-}
-
-# Remove linux-firmware package if dependent kernels were removed
-# cp -al FIRMWARE_DIR_LINUX_NEW to /lib/firmware if all was successful
-# Use mark placed by 310_kernel_related.sh
-
-# In future we COULD use pkg_apt_op_analysis to check whether removing
-# linux-firmware WOULD remove any other package instead of using marker
-[[ -f "$REMASTER_DIR"/ubuntu_kernels_removed ]] && {
-    # No dependent kernels left - we can remove linux-firmware safely
-
-    DEBIAN_FRONTEND=noninteractive $APT_CMD remove -y --purge linux-firmware 1>/dev/null 2>&1 && {
-        echo "Removed package linux-firmware"
-        mv /lib/firmware /lib/firmware-old-remaster || true
-        mv $FIRMWARE_DIR_LINUX_NEW /lib/firmware 
-    } || {
-        echo "Could not remove package linux-firmware"
+[[ "$FIRMWARE_UPDATE_FIRMWARE_GIT_UBUNTU" = "yes" || \
+    "$FIRMWARE_UPDATE_FIRMWARE_GIT_LINUX" = "yes" || \
+    "$FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL" = "yes" \
+    ]] && {
+        install_git_if_required || exit $FAILED_EXIT_CODE
     }
-} || {
-    # linux-firmware needs to be held - no updates or deletion
-    apt-mark hold linux-firmware
-    echo "linux-firmware package held"
-    mv /lib/firmware /lib/firmware-old-remaster
-    mv $FIRMWARE_DIR_LINUX_NEW /lib/firmware
-}
+
+export FIRMWARE_UPDATE_FIRMWARE_GIT_UBUNTU FIRMWARE_UPDATE_FIRMWARE_GIT_LINUX FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL
+$UPDATE_FIRMWARE_SCRIPT /lib || exit $FAILED_EXIT_CODE
+\cp $UPDATE_FIRMWARE_SCRIPT "$REMASTER_DIR"/
---
+++ remaster/chroot/update_firmware/update_firmware.sh
@@ -0,0 +1,374 @@
+#!/bin/bash
+# $1: LIB_DIR - should contain directory named 'firmware'
+#
+# LIB_DIR should contain directory named 'firmware' - current firmware
+# LIB_DIR/firmware is updated IN PLACE - so non-standard files (like edid/* etc) will remain
+# In case of any failure, LIB_DIR/firmware will be unchanged
+#
+# 3 environment variables are used if present to decide which updates to pull
+FIRMWARE_UPDATE_FIRMWARE_GIT_UBUNTU=${FIRMWARE_UPDATE_FIRMWARE_GIT_UBUNTU:-yes}
+FIRMWARE_UPDATE_FIRMWARE_GIT_LINUX=${FIRMWARE_UPDATE_FIRMWARE_GIT_LINUX:-yes}
+FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL=${FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL:-yes}
+
+
+PROG_NAME=$(basename $0)
+set -eu -o pipefail
+
+
+function show_usage() {
+    echo "- Usage:
+    ${PROG_NAME} [-h|--help] <LIB_DIR>
+        -h|--help : Show this help and exit
+
+- LIB_DIR:
+    Running as root, to update /lib/firmware, LIB_DIR should be /lib
+
+    LIB_DIR should contain directory named firmware - current firmware
+    Current firmware will be backed up to LIB_DIR/firmware-backups/firmware-YYYYMMDD_HHMMSS
+    Most recent backed up firmware will be LIB_DIR/firmware-backups/latest (symlink)
+    On successful update, new updated firmware will be in LIB_DIR/firmware
+    In case of any failure, LIB_DIR/firmware will be unchanged
+
+- Sample output:
+
+    ubuntu-firmware-git   : Files added / updated :   889 (  379M)
+    linux-firmware-git    : Files added / updated :   181 (   25M)
+    iwlwifi-firmware-git  : Files added / updated :    13 ( -108K)
+    Total                 : Files added / updated :  1945 (  402M)
+    Started  with  1945 files
+    Finished with  2563 files
+    Original firmware backed up to /lib/firmware-backups/firmware-20210422_042105
+
+- Needs: 
+    git (git)
+    bash (bash) (not sh / ksh / zsh etc)
+    coreutils (df, numfmt)
+    grep (grep)
+    gawk (awk)
+    sed (sed)
+- Needs Internet access (for git clone)
+- Takes about 5 - 6 mins to complete
+"
+}
+
+function partition_bytes_used() {
+    # $1: directory - defaults to $(pwd)
+    # Outputs partition (df) bytes used to stdout
+    # Returns:
+    #   0 if successful (part_dir valid or unspecified ($(pwd) used)
+    #   1 if part_dir invalid (outputs nothing)
+    local part_dir=
+    [[ $# -gt 0 ]] && part_dir=$1 || part_dir=$(pwd)
+    [[ -d "$part_dir" ]] || return 1
+    df --sync -B1 --output=used "$part_dir" | sed -e '1d'
+}
+
+function create_backup_dir() {
+    # $1: parent directory - must exist
+    # Outputs newly created backup directory to stdout
+    # Returns: 0 if successful; 1 otherwise
+    [[ $# -lt 1 ]] && return 1
+    local parent_dir=$1
+    [[ -n "$parent_dir" ]] && [[ -d "$parent_dir" ]] || return 1
+    local TS=$(date '+%Y%m%d_%H%M%S')
+    while true
+    do
+        mkdir "$parent_dir"/firmware-${TS} && break || {
+            sleep 2
+            TS=$(date '+%Y%m%d_%H%M%S')
+            continue
+        }
+    done
+    echo "$parent_dir"/firmware-${TS}
+}
+
+function firmware_update_from_git(){
+    # $1: Existing firmware dir - will be UPDATED !
+    # $2: Directory in which to create tmp dir - will be created if it doesn't exist
+    # $3: git clone URL
+    # Echoes number of FILES with contents updated to stdout
+    local tmp_parent_dir=
+    local tmp_dir=
+    local tmp_parent_created=no
+    [[ $# -lt 3 ]] && return 1
+    [[ -d "$1" ]] || return 2
+    local target=$(readlink -e "$1")
+    tmp_parent_dir=$(readlink -m "$2")
+    local git_url=$3
+    local num_files_updated=0
+    if [[ ! -d "$tmp_parent_dir" ]]; then
+        mkdir -p "$tmp_parent_dir" || return 3
+        tmp_parent_created=yes
+    fi
+
+    function cleanup_firmware_update_from_git(){
+        [[ -n "$tmp_dir" ]] && [[ -d "$tmp_dir" ]] && {
+            \rm -rf "$tmp_dir"
+        }
+        [[ "$tmp_parent_created" = "yes" ]] && {
+            [[ -n "$tmp_parent_dir" ]] && [[ -d "$tmp_parent_dir" ]] && {
+                \rm -rf "$tmp_parent_dir"
+            }
+        }
+    }
+    trap cleanup_firmware_update_from_git RETURN 1 2 3 15
+
+    tmp_dir=$(mktemp -d -p "$tmp_parent_dir" "update_firmware_from_git.XXXXXX")
+    git clone --quiet --depth 1 "$git_url" "$tmp_dir"/new || return 4
+    \rm -rf "$tmp_dir"/new/.git
+
+    cd "$tmp_dir"/new
+    num_files_updated=$(find -type f -printf '%P\n' | while read f
+    do
+        [[ -e "$target"/"$f" ]] && {
+            diff --brief "$f" "$target"/"$f" 1>/dev/null || {
+                # Update existing file
+                \cp -af --parents "$f" "$target" 1>/dev/null
+                echo "Updated"
+            }
+        } || {
+            # New file
+            \cp -af --parents "$f" "$target" 1>/dev/null
+            echo "New"
+        }   
+    done | wc -l)
+    cd - 1>/dev/null
+
+    echo $num_files_updated
+    rm -rf "$tmp_dir"/new
+}
+
+function firmware_update_iwlwifi_from_git() {
+    # $1: Existing firmware dir - will be UPDATED !
+    # $2: Directory in which to create tmp dir - will be created if it doesn't exist
+    # Echoes number of FILES with contents updated to stdout
+    local tmp_parent_dir=
+    local tmp_dir=
+    local tmp_parent_created=no
+    [[ $# -lt 2 ]] && return 1
+    [[ -d "$1" ]] || return 2
+    local target=$(readlink -e "$1")
+    tmp_parent_dir=$(readlink -m "$2")
+    local git_url='https://git.kernel.org/pub/scm/linux/kernel/git/iwlwifi/linux-firmware.git'
+    local num_files_updated=0
+    if [[ ! -d "$tmp_parent_dir" ]]; then
+        mkdir -p "$tmp_parent_dir" || return 3
+        tmp_parent_created=yes
+    fi
+
+    function cleanup_firmware_update_iwlwifi_from_git(){
+        [[ -n "$tmp_dir" ]] && [[ -d "$tmp_dir" ]] && {
+            \rm -rf "$tmp_dir"
+        }
+        [[ "$tmp_parent_created" = "yes" ]] && {
+            [[ -n "$tmp_parent_dir" ]] && [[ -d "$tmp_parent_dir" ]] && {
+                \rm -rf "$tmp_parent_dir"
+            }
+        }
+    }
+    trap cleanup_firmware_update_iwlwifi_from_git RETURN 1 2 3 15
+
+    tmp_dir=$(mktemp -d -p "$tmp_parent_dir" "update_firmware_iwlwifi_from_git.XXXXXX")
+    git clone --quiet --depth 1 "$git_url" "$tmp_dir"/new || return 4
+    \rm -rf "$tmp_dir"/new/.git
+    mkdir -p "$target"/intel || return 5
+
+    # iwlwifi-*.ucode
+    cd "$tmp_dir"/new
+    num_files_updated=$(( $num_files_updated + $(find -maxdepth 1 -type f -name 'iwlwifi-*.ucode' -printf '%P\n' | while read f
+    do
+        [[ -e "$target"/"$f" ]] && {
+            diff --brief "$f" "$target"/"$f" 1>/dev/null || {
+                # Update existing file
+                \cp -af --parents "$f" "$target" 1>/dev/null
+                echo "Updated"
+            }
+        } || {
+            # New file
+            \cp -af --parents "$f" "$target" 1>/dev/null
+            echo "New"
+        }   
+    done | wc -l ) ))
+    cd - 1>/dev/null
+
+    # intel sub-directory
+    [[ -d "$tmp_dir"/new/intel ]] && {
+        cd "$tmp_dir"/new/intel
+        num_files_updated=$(( $num_files_updated + $(find -type f -printf '%P\n' | while read f
+        do
+            [[ -e "$target"/intel/"$f" ]] && {
+                diff --brief "$f" "$target"/intel/"$f" 1>/dev/null || {
+                    # Update existing file
+                    \cp -af --parents "$f" "$target"/intel 1>/dev/null
+                    echo "Updated"
+                }
+            } || {
+                # New file
+                \cp -af --parents "$f" "$target"/intel 1>/dev/null
+                echo "New"
+            }   
+        done | wc -l ) ))
+    }
+    cd - 1>/dev/null
+
+    echo $num_files_updated
+    rm -rf "$tmp_dir"/new
+}
+
+function firmware_update_all() {
+    # $1: LIB_DIR - should contain dir firmware
+
+    local UBUNTU_GIT_URL='git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux-firmware'
+    local LINUX_GIT_URL='git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git'
+    [[ $# -lt 1 ]] && {
+        show_usage
+        return 1
+    }
+
+    local LIB_DIR=
+
+    while [[ $# -gt 0 ]];
+    do
+        case "$1" in
+            h|--help)
+                show_usage
+                return 0
+                ;;
+            *)
+                LIB_DIR=$1
+                shift
+                ;;
+        esac
+    done
+
+    [[ -z "$LIB_DIR" ]] && {
+        show_usage
+        return 1
+    }
+    [[ -d "$LIB_DIR" ]] || {
+        >&2 echo "Dir does not exist: $LIB_DIR"
+        show_usage
+        return 2
+    }
+    LIB_DIR=$(readlink -e "$LIB_DIR")
+    BAK_DIR="$LIB_DIR"/firmware-backups
+    mkdir -p "$BAK_DIR" || return 3
+
+    local FIRMWARE_DIR="$LIB_DIR"/firmware
+    local FIRMWARE_ORIG_DIR=$(create_backup_dir "$BAK_DIR")
+    local FIRMWARE_NEW_DIR="$LIB_DIR"/firmware.new
+    local TMP_DIR="$LIB_DIR"/tmp
+
+    cd "$LIB_DIR"
+    local ORIGINAL_SIZE=$(partition_bytes_used "$LIB_DIR")
+    local TOT_UPDATED_FILES=0
+    local TOT_STARTING_FILES=$(find "$FIRMWARE_DIR" -type f | wc -l)
+    local UPDATE_DONE=no
+
+
+    # firmware dir -------------------------------------------------------
+    [[ -d "$FIRMWARE_DIR" ]] || {
+        >&2 echo "Directory does not exist: $FIRMWARE_DIR"
+        return 4
+    }
+
+    # firmware.orig dir --------------------------------------------------
+    \cp -al "$FIRMWARE_DIR"/. "$FIRMWARE_ORIG_DIR"/.
+    ( 
+        cd "$FIRMWARE_ORIG_DIR"
+        \rm -f WHENCE.* date.*
+        local find_output=$(find . -type f -printf '%TY%Tm%Td_%TH%TM%TS %P\n' | sort -nr | head -1)
+        local newest_file=$(echo "$find_output" | cut -d' ' -f2)
+        local file_ts=$(echo "$find_output" | cut -d' ' -f1)
+        echo "$file_ts" > date.orig
+        touch --reference="$newest_file" date.orig
+        [[ -f WHENCE ]] && \mv -f WHENCE WHENCE.orig
+        chmod go-w date.orig
+        cd - 1>/dev/null 
+    )
+
+    # firmware.new dir ---------------------------------------------------
+    [[ -e "$FIRMWARE_NEW_DIR" ]] && {
+        echo "Removing directory: $FIRMWARE_NEW_DIR"
+        \rm -rf "$FIRMWARE_NEW_DIR" || return 4
+    }
+    \cp -al "$FIRMWARE_DIR" "$FIRMWARE_NEW_DIR"
+
+    local NEW_SIZE=$ORIGINAL_SIZE
+    local ORIG_SIZE_1=$ORIGINAL_SIZE
+    local UPDATED_FILES_1=0
+    local ret=0
+
+    # ubuntu-git ---------------------------------------------------------
+    [[ "$FIRMWARE_UPDATE_FIRMWARE_GIT_UBUNTU" = "yes" ]] && {
+        UPDATE_DONE=yes
+        UPDATED_FILES_1=$(firmware_update_from_git "$FIRMWARE_NEW_DIR" "$TMP_DIR" "$UBUNTU_GIT_URL" || {
+            ret=$?
+            >&2 echo "firmware_update_from_git failed: $UBUNTU_GIT_URL ($ret)"
+            \rm -rf "$FIRMWARE_NEW_DIR" "$FIRMWARE_ORIG_DIR"
+            return $(( 4 + $ret ))
+        })
+        [[ -f "$FIRMWARE_NEW_DIR"/WHENCE ]] && \mv -f "$FIRMWARE_NEW_DIR"/WHENCE "$FIRMWARE_NEW_DIR"/WHENCE.ubuntu
+        date '+%Y%m%d-%H%M%S' > "$FIRMWARE_NEW_DIR"/date.ubuntu
+        TOT_UPDATED_FILES=$(( $TOT_UPDATED_FILES + $UPDATED_FILES_1 ))
+        ORIG_SIZE_1=$NEW_SIZE
+        NEW_SIZE=$(partition_bytes_used "$LIB_DIR")
+        printf "ubuntu-firmware-git   : Files added / updated : %5d (%6s)\n" $UPDATED_FILES_1 $( echo "$(( $NEW_SIZE - $ORIG_SIZE_1 ))" | numfmt --to=iec )
+    }
+
+    # linux-firmware-git --------------------------------------------------
+    [[ "$FIRMWARE_UPDATE_FIRMWARE_GIT_LINUX" = "yes" ]] && {
+        UPDATE_DONE=yes
+        UPDATED_FILES_1=$(firmware_update_from_git "$FIRMWARE_NEW_DIR" "$TMP_DIR" "$LINUX_GIT_URL" || {
+            ret=$?
+            >&2 echo "firmware_update_from_git failed: $LINUX_GIT_URL ($ret)"
+            \rm -rf "$FIRMWARE_NEW_DIR" "$FIRMWARE_ORIG_DIR"
+            return $(( 4 + $ret ))
+        })
+        [[ -f "$FIRMWARE_NEW_DIR"/WHENCE ]] && \mv -f "$FIRMWARE_NEW_DIR"/WHENCE "$FIRMWARE_NEW_DIR"/WHENCE.linux-firmware
+        date '+%Y%m%d-%H%M%S' > "$FIRMWARE_NEW_DIR"/date.linux-firmware
+        TOT_UPDATED_FILES=$(( $TOT_UPDATED_FILES + $UPDATED_FILES_1 ))
+        ORIG_SIZE_1=$NEW_SIZE
+        NEW_SIZE=$(partition_bytes_used "$LIB_DIR")
+        printf "linux-firmware-git    : Files added / updated : %5d (%6s)\n" $UPDATED_FILES_1 $( echo "$(( $NEW_SIZE - $ORIG_SIZE_1 ))" | numfmt --to=iec )
+    }
+
+    # iwlwifi-git --------------------------------------------------------
+    [[ "$FIRMWARE_UPDATE_FIRMWARE_GIT_INTEL" = "yes" ]] && {
+        UPDATE_DONE=yes
+        UPDATED_FILES_1=$(firmware_update_iwlwifi_from_git "$FIRMWARE_NEW_DIR" "$TMP_DIR" || {
+            ret=$?
+            >&2 echo "firmware_update_iwlwifi_from_git failed: ($ret)"
+            \rm -rf "$FIRMWARE_NEW_DIR" "$FIRMWARE_ORIG_DIR"
+            return $(( 4 + $ret ))
+        })
+        [[ -f "$FIRMWARE_NEW_DIR"/WHENCE ]] && \mv -f "$FIRMWARE_NEW_DIR"/WHENCE "$FIRMWARE_NEW_DIR"/WHENCE.iwlwifi
+        date '+%Y%m%d-%H%M%S' > "$FIRMWARE_NEW_DIR"/date.iwlwifi
+        TOT_UPDATED_FILES=$(( $TOT_UPDATED_FILES + $UPDATED_FILES_1 ))
+        ORIG_SIZE_1=$NEW_SIZE
+        NEW_SIZE=$(partition_bytes_used "$LIB_DIR")
+        printf "iwlwifi-firmware-git  : Files added / updated : %5d (%6s)\n" $UPDATED_FILES_1 $( echo "$(( $NEW_SIZE - $ORIG_SIZE_1 ))" | numfmt --to=iec )
+    }
+
+    [[ "$UPDATE_DONE" = "yes" ]] && {
+        # Replace FIRMWARE_DIR with FIRMWARE_NEW_DIR -------------------------
+        \rm -rf $FIRMWARE_DIR
+        mv $FIRMWARE_NEW_DIR $FIRMWARE_DIR
+        # create symlink 'latest'
+        \rm -rf "$BAK_DIR"/latest && ln -sf $(basename "$FIRMWARE_ORIG_DIR") "$BAK_DIR"/latest || true
+
+        local NEW_SIZE=$(partition_bytes_used "$LIB_DIR")
+        printf "Total                 : Files added / updated : %5d (%6s)\n" $TOT_UPDATED_FILES $( echo "$(( $NEW_SIZE - $ORIGINAL_SIZE ))" | numfmt --to=iec )
+        printf "Started  with %5d files\n" $TOT_STARTING_FILES
+        printf "Finished with %5d files\n" $(find "$FIRMWARE_DIR" -type f | wc -l)
+        echo "Original firmware backed up to $FIRMWARE_ORIG_DIR"
+    } || {
+        \rm -rf "$FIRMWARE_NEW_DIR" "$FIRMWARE_ORIG_DIR"
+    }
+}
+
+# ------------------------------------------------------------------------
+# Actual script starts after this
+# ------------------------------------------------------------------------
+
+firmware_update_all $@ || exit $?
